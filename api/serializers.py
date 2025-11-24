c9f51c56a6cd04c3f2916b1e8791f6794d07eb6e
YasSantrb <yasmimsantana119@gmail.com>
2025-11-24 15:18:19 -0300

    feat: adicionando funções de cadastro, login e superusuario.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,45 @@
 from rest_framework import serializers
 from .models import Experimento
+from .models import UserProfile
+from .models import User
+from django.db import IntegrityError
 
 class ExperimentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experimento
         fields = '__all__'
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['cidade', 'estado']
+        
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email', 'profile']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password', None)
+        
+        try:
+            user = User.objects.create(**validated_data)
+            if password:
+                user.set_password(password)
+                user.save()
+            UserProfile.objects.create(user=user, **profile_data)
+            
+            return user
+            
+        except IntegrityError as e:
+            if 'user' in locals():
+                 user.delete()
+            raise serializers.ValidationError({"detail": "Já existe um usuário com este nome de usuário ou e-mail."}) 
+            
+        except Exception as e:
+            if 'user' in locals():
+                 user.delete()
+            raise serializers.ValidationError({"error": "Erro desconhecido ao criar usuário: " + str(e)})
--- api/views.py
+++ api/views.py
@@ -3,22 +3,64 @@ from rest_framework.response import Response
 from .models import Experimento
 from django.shortcuts import get_object_or_404
 from .serializers import ExperimentoSerializer
-
+from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions, status
+from .models import UserProfile
 class ExperimentoCreate(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
-        experiementos = Experimento.objects.all()
-        serializer = ExperimentoSerializer(experiementos, many=True)
+        experimentos = Experimento.objects.filter(usuario=request.user)
+        serializer = ExperimentoSerializer(experimentos, many=True)
         return Response(serializer.data)
     
     def post(self, request):
         serializer = ExperimentoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            serializer.save(usuario=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ExperimentoDetalhe(APIView):
     def get(self, request, pk):
         experimento = get_object_or_404(Experimento, pk=pk)
         serializer = ExperimentoSerializer(experimento)
         return Response(serializer.data)
+    
+    
+class RegistroUsuarioView(APIView):
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response( data= {
+                'mensagem': 'Usuário criado com sucesso!',
+                'token': token.key,
+                'usuario': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        from django.contrib.auth import authenticate
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response( data= {
+                'token': token.key,
+                'usuario_id': user.id,
+                'username': user.username,
+            })
+        return Response({'erro': 'Credenciais inválidas.'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    def get(self, request):
+        return Response({'mensagem': 'Envie username e password via post para obter o TOKEN.'})
