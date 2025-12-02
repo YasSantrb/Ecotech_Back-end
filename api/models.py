616bca74bfc962d558511025f6bcfaed8712ad07
YasSantrb <yasmimsantana119@gmail.com>
2025-12-02 11:38:00 -0300

    feat: adicionando o campo número no modelo de pontos de coleta.

--- api/models.py
+++ api/models.py
@@ -24,6 +24,7 @@ class PontosColeta(models.Model):
     cep = models.CharField(max_length=9)
     telefone = models.CharField(max_length=15)
     horario_funcionamento = models.CharField(max_length=100)
+    numero = models.IntegerField(null=True)
     criado_em = models.DateTimeField(auto_now_add = True)
     
     def __str__(self):
