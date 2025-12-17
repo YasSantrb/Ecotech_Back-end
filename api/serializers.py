0d6a9848c4aa5d3247d46f5e877d62684f17bb4e
YasSantrb <yasmimsantana119@gmail.com>
2025-12-17 11:33:22 -0300

    atualizando o main

--- api/serializers.py
+++ api/serializers.py
@@ -26,13 +26,10 @@ def validar_cnpj(value):
         return value
 class UserProfileSerializer(serializers.ModelSerializer):
     cpf_cnpj = serializers.CharField(write_only=True, required=False)
-    cpf = serializers.CharField(read_only=True)
-    cnpj = serializers.CharField(read_only=True)
-    tipo_usuario = serializers.CharField(read_only=True)
     class Meta:
         model = UserProfile
-        fields = ['cpf', 'cnpj', 'telefone', 'cep', 'cpf_cnpj', 'tipo_usuario', 'criado_em']
-        read_only_fields = ['criado_em']
+        fields = ['cpf', 'cnpj', 'telefone', 'cep', 'cpf_cnpj', 'tipo_usuario']
+        read_only_fields = ['cpf', 'cnpj', 'tipo_usuario']
         
     def validate(self, data):
         identificador = data.pop('cpf_cnpj', None)
